8950b84d294ba635dc2125fcdbecb8caae6484f7
gowdagnanavid-ctrl <gowdagnanavid@gmail.com>
2026-04-21 18:19:31 +0530

    Add files via upload

---
+++ boxpattern.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class boxpattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("*");
+                } else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ hallowpyramid.java
@@ -0,0 +1,19 @@
+package day5;
+public class hallowpyramid {
+    public static void main(String[] args) {
+        int rows = 4;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                if (i == 1 || i == rows || k == 1 || k == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
